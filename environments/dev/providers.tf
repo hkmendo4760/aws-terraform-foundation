292091383d55bc77f265304607acf2aa6e392521
hkmendo4760 <227089387+hkmendo4760@users.noreply.github.com>
2026-05-16 23:33:27 +0000

    added global sts

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -17,9 +17,11 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = var.aws_region
+
+  # Force the provider to resolve authentication tokens via the global STS endpoint
+  sts_region_endpoints = "global"
 
   # Enforcement of standardized naming conventions across your live dev account
   default_tags {
